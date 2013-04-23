0a10bf37cd0c84d48377cf8959c90ca24c09f982
florianmski <florian.pub@gmail.com>
2013-04-23 14:20:16 +0200

    add .gitmodules

--- Application/src/com/polytech/polydraw/ui/activities/GameActivity.java
+++ Application/src/com/polytech/polydraw/ui/activities/GameActivity.java
@@ -17,15 +17,15 @@ public class GameActivity extends BaseActivity
 		
 		setContentView(R.layout.activity_empty);
 		
-//        SlidingMenu menu = new SlidingMenu(this);
-//        menu.setMode(SlidingMenu.LEFT);
-//        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
-//        menu.setShadowWidthRes(R.dimen.shadow_width);
-//        menu.setShadowDrawable(R.drawable.shadow);
-//        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
-//        menu.setFadeDegree(0.35f);
-//        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
-//        menu.setMenu(R.layout.menu);
+        SlidingMenu menu = new SlidingMenu(this);
+        menu.setMode(SlidingMenu.LEFT);
+        menu.setTouchModeAbove(SlidingMenu.TOUCHMODE_FULLSCREEN);
+        menu.setShadowWidthRes(R.dimen.shadow_width);
+        menu.setShadowDrawable(R.drawable.shadow);
+        menu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+        menu.setFadeDegree(0.35f);
+        menu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+        menu.setMenu(R.layout.menu);
 		
 		getFragmentManager()
 		.beginTransaction()
